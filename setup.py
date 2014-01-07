488e6025f766736bea100e3c93d83ee2f3bc85fe
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2014-01-07 17:12:05 +0100

    ADD setup.py script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Sparse Filtering',
+      version='1.0',
+      description='Unsupervised feature learning based on sparse-filtering',
+      author='Jan Hendrik Metzen',
+      author_email='jhm@informatik.uni-bremen.de',
+      url='https://github.com/jmetzen/sparse-filtering',
+      py_modules = ['sparse_filtering']
+     )
